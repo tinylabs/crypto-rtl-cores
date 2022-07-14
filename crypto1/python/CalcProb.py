d944d42b8c6844a0821762b719949d60d333dc22
Tiny Labs Inc <elliot@tinylabs.io>
2022-07-14 16:34:36 -0600

    Add key rewind

--- crypto1/python/CalcProb.py
+++ crypto1/python/CalcProb.py
@@ -17,6 +17,20 @@ import json
 ilookup = [[0, 2, 4, 5, 6, 7, 8, 9, 10, 12, 19, 21, 23, 24, 25, 28],
            [1, 3, 11, 13, 14, 15, 16, 17, 18, 20, 22, 26, 27, 29, 30, 31]]
 
+def XOR (key):
+    return key[47] ^ key[42] ^ key[38] ^ key[37] ^ key[35] ^ key[33] ^ \
+        key[32] ^ key[30] ^ key[28] ^ key[23] ^ key[22] ^ key[20] ^ \
+        key[18] ^ key[12] ^ key[8] ^ key[6] ^ key[5] ^ key[4]
+
+# Rewind state 45 cycles
+def Rewind (key):
+    state = int2binarr (key, 48)[::-1]
+    for n in range (45):
+        a = state[0]
+        state = state[1:48] + [0]
+        state[47] = XOR(state) ^ a
+    return binarr2int (state[::-1])
+
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser ()
@@ -28,6 +42,8 @@ if __name__ == '__main__':
                          help='Get even/odd index for bitstream')
     parser.add_argument ('--get_random', action='store_true',
                          help='Get random valid key and indices')
+    parser.add_argument ('--rewind', type=str,
+                         help='Rewind key 45 cycles')
     args = parser.parse_args ()
 
 
@@ -106,6 +122,11 @@ if __name__ == '__main__':
         # Use monte-carlo random num gen to create key
         key = hex(random.randint (1, 2**48))
         args.get_idx = key
+
+    elif args.rewind:
+        # Rewind key
+        key = int (args.rewind, 0)
+        print ('Key={}'.format (hex(Rewind (key))))
         
     # Get even/odd index for key
     if args.get_idx:
